a9edf89ceb5ab84b00c6c6b129700fd8550a2df4
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-06-23 04:18:12 +0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import './assets/style.css'
+import 'boxicons/css/boxicons.min.css'
+
+import { createRouter, createWebHistory } from 'vue-router'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home.vue')
+  },
+ {
+    path: '/activity',
+    name: 'Visualization',
+    component: () => import('@/views/Visualizer.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash
+      }
+    }
+  }
+})
+
+createApp(App).use(router).mount('#app')
