5ea2712ba61cc48af72adb5c0f450ae88535bd33
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-06-25 01:35:12 +0800

    Added Demo Page

--- src/main.js
+++ src/main.js
@@ -15,6 +15,11 @@ const routes = [
     path: '/activity',
     name: 'Visualization',
     component: () => import('@/views/Visualizer.vue')
+  },
+   {
+    path: '/demo',
+    name: 'Demo',
+    component: () => import('@/views/Demo.vue')
   }
 ]
 
